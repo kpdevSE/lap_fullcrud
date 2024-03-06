5eac5ab3e25eb1d04df8eae4f57f57322cacb4db
kpdevSE <kanishkapasindu6@gmail.com>
2024-03-06 18:11:13 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const FormDataModel = require("./model/FormData");
 
 //importing dot env
 require("dotenv").config();
@@ -47,6 +48,38 @@ mongoose
 app.get("/home", (req, resp) => {
   resp.json({
     message: "Hello World",
+  });
+});
+app.post("/register", (req, res) => {
+  // To post / insert data into database
+
+  const { email, password } = req.body;
+  FormDataModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      res.json("Already registered");
+    } else {
+      FormDataModel.create(req.body)
+        .then((log_reg_form) => res.json(log_reg_form))
+        .catch((err) => res.json(err));
+    }
+  });
+});
+app.post("/login", (req, res) => {
+  // To find record from the database
+  const { email, password } = req.body;
+  FormDataModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      // If user found then these 2 cases
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("Wrong password");
+      }
+    }
+    // If user not found then
+    else {
+      res.json("No records found! ");
+    }
   });
 });
 
